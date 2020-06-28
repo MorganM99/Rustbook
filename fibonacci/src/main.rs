058dc3826e221927be4920556a2411dc3abb1dd0
MorganM99 <morganmckay1999@gmail.com>
2020-06-28 23:00:22 +0100

    Final tweak before push

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,25 +1,22 @@
 use std::env;
 
-fn fibonacci(n:u32) -> u32{
-    let mut res:u32=0;
-    if n==0{
+fn fibonacci(n: u32) -> u32 {
+    let mut res: u32 = 0;
+    if n == 0 {
         return res;
-    }
-    else if n==1
-    {
-        res=1;
+    } else if n == 1 {
+        res = 1;
         return res;
-    }
-    else{
-        res=fibonacci(n-1)+fibonacci(n-2);
+    } else {
+        res = fibonacci(n - 1) + fibonacci(n - 2);
         return res;
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let n=&args[1];
-    let n:u32=n.trim().parse().expect("Please enter a number!");
-    let res=fibonacci(n);
-    println!("The {}th fibonacci number is: {}",n,res);
+    let n = &args[1];
+    let n: u32 = n.trim().parse().expect("Please enter a number!");
+    let res = fibonacci(n);
+    println!("The {}th fibonacci number is: {}", n, res);
 }
